796401a512f30e2a37f0694526d0e6f0d7820b35
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 11:38:58 +0100

    Properly set env vars from dependencies

--- hooks/debug_setter.py
+++ hooks/debug_setter.py
@@ -4,43 +4,45 @@ import os
 def env_prepend(var, val, sep=os.pathsep):
     os.environ[var] = val + (sep + os.environ[var] if var in os.environ else "")
 
+
 def pre_build(output, conanfile, **kwargs):
     assert conanfile
     # Set debug prefix flags
-    if not (
-        hasattr(conanfile, "source_folder")            # Needs source directory
-        and "build_type" in conanfile.settings.fields  # Needs build_type setting
-        and conanfile.settings.build_type == "Debug"   # Only for Debug build_type
-    ):
+    if not (hasattr(conanfile, "source_folder")  # Needs source directory
+            and "build_type" in conanfile.settings.fields  # Needs build_type setting
+            and conanfile.settings.build_type == "Debug"  # Only for Debug build_type
+            ):
         return
     env_prepend(
         "CFLAGS",
         "-fdebug-prefix-map=%s=%s" % (conanfile.source_folder, conanfile.name),
         " ",
     )
+
     env_prepend(
         "CXXFLAGS",
         "-fdebug-prefix-map=%s=%s" % (conanfile.source_folder, conanfile.name),
         " ",
     )
 
+
 def post_package(output, conanfile, conanfile_path, **kwargs):
     assert conanfile
     # Copy sources to package
-    if not (
-        "build_type" in conanfile.settings.fields    # Needs build_type setting
-        and conanfile.settings.build_type == "Debug" # Only for Debug build_type
-    ):
+    if not ("build_type" in conanfile.settings.fields  # Needs build_type setting
+            and conanfile.settings.build_type == "Debug"  # Only for Debug build_type
+            ):
         return
     for ext in ("c", "cpp", "cpp", "h", "hpp", "hxx"):
         conanfile.copy("*." + ext, "src")
 
+
 def pre_package_info(output, conanfile, reference, **kwargs):
     assert conanfile
     # Set source mapping env var
-    if not (
-        "build_type" in conanfile.settings.fields    # Needs build_type setting
-        and conanfile.settings.build_type == "Debug" # Only for Debug build_type
-    ):
+    if not ("build_type" in conanfile.settings.fields  # Needs build_type setting
+            and conanfile.settings.build_type == "Debug"  # Only for Debug build_type
+            ):
         return
     conanfile.env_info.SOURCE_MAP.append("%s|%s" % (conanfile.name, os.path.join(conanfile.package_folder, "src")))
+    conanfile.cpp_info.srcsdirs = ["src"]
--- hooks/environment_setter.py
+++ hooks/environment_setter.py
@@ -3,25 +3,23 @@ import os
 from conans import tools
 
 
+def env_prepend(var, val, sep=os.pathsep):
+    os.environ[var] = val + (sep + os.environ[var] if var in os.environ else "")
+
+
+def pre_build(output, conanfile, reference, **kwargs):
+    env_prepend("PKG_CONFIG_PATH", conanfile.build_folder, ":")
+
+
 def pre_package_info(output, conanfile, reference, **kwargs):
     assert conanfile
-    # Find bin, lib and prefix paths
-    bin_paths = []
-    lib_paths = []
-    prefix_paths = []
-    for pkg_name, cpp_info in conanfile.deps_cpp_info.dependencies:
-        prefix_paths.append(cpp_info.rootpath)
-        lib_path = os.path.join(cpp_info.rootpath, "lib")
-        if os.path.isdir(lib_path):
-            lib_paths.append(lib_path)
-        bin_path = os.path.join(cpp_info.rootpath, "bin")
-        if os.path.isdir(bin_path):
-            bin_paths.append(bin_path)
 
-    # Set PATH, LD_LIBRARY_PATH and CMAKE_PREFIX_PATH env vars
-    for bin_path in bin_paths:
+    conanfile.env_info.CMAKE_PREFIX_PATH.append(conanfile.cpp_info.rootpath)
+
+    bin_path = os.path.join(conanfile.cpp_info.rootpath, "bin")
+    if os.path.isdir(bin_path):
         conanfile.env_info.PATH.append(bin_path)
-    for lib_path in lib_paths:
+
+    lib_path = os.path.join(conanfile.cpp_info.rootpath, "lib")
+    if os.path.isdir(lib_path):
         conanfile.env_info.LD_LIBRARY_PATH.append(lib_path)
-    for prefix_path in prefix_paths:
-        conanfile.env_info.CMAKE_PREFIX_PATH.append(prefix_path)
