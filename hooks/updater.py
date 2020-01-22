2ee8277133af6901333aae142559781c0e0cb46b
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-22 11:40:53 +0100

    Use https connection for updater hook

--- hooks/updater.py
+++ hooks/updater.py
@@ -2,4 +2,4 @@ import os
 
 
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
-    os.system("conan config install git@gitlab.com:aivero/public/conan/conan-config.git")
+    os.system("conan config install https://gitlab.com/aivero/public/conan/conan-config.git")
