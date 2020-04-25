cceb463270fb98198f4512b57adf0cd67e6e5c5a
Ulrich Eck <ueck@net-labs.de>
2020-04-25 17:38:13 +0200

    Update url_setter.py

--- hooks/url_setter.py
+++ hooks/url_setter.py
@@ -1,4 +1,4 @@
 def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     # Set url if it does not exist
     if not hasattr(conanfile, "url"):
-        conanfile.url = "https://github.com/aivero/conan-%s" % conanfile.name
+        conanfile.url = "https://github.com/TUM-CONAN/conan-%s" % conanfile.name
